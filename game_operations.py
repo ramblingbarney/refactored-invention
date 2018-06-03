f9e958abe87980de1798eea00cab20b59a817179
Conor O'Dwyer <c9dw5er@protonmail.com>
2018-06-03 22:56:58 +0100

    - updated for fixes from pep8/pycodestyle 'game_operations'

--- game_operations.py
+++ game_operations.py
@@ -9,11 +9,12 @@ from fuzzywuzzy import process
 # this is a python implenentation of the published theory
 # https://en.wikipedia.org/wiki/Levenshtein_distance
 # I have used a library provided by SeatGeek
- #https://github.com/seatgeek/fuzzywuzzy
-'''levenshtein string comparison'''
+# https://github.com/seatgeek/fuzzywuzzy
 
+'''levenshtein string comparison'''
 levenshtein_score = lambda to_be_scored_string, answer: fuzz.ratio(
     to_be_scored_string, answer)
+
 
 def create_list_all_players_names(all_game_players):
 
@@ -32,11 +33,12 @@ def create_list_all_players_names(all_game_players):
 
     # sort names dictionary by value
     sorted_names_dict = sorted(names_dict.items(),
-        key = itemgetter(1), reverse = True)
+                            key = itemgetter(1), reverse = True)
 
     ordered_player_names = list(OrderedDict(sorted_names_dict).keys())
 
     return ordered_player_names
+
 
 def create_song_list_each_name(names):
 
@@ -53,14 +55,15 @@ def create_song_list_each_name(names):
         # select all songs by person, if no songs 'None' is returned
         # 'first' returns the first result, 'all' returns all results
         raw_result = file_operations.search_from_file(
-        'data/song_scores.txt', name,1)
+                    'data/song_scores.txt', name,1)
 
         if ( raw_result is not None ):
 
             for key, value in raw_result.items():
 
                 name_individual_songs.append(
-                    "{0} - {1}".format(value.split(",")[1],value.split(",")[2]))
+                    "{0} - {1}".format(value.split(",")[1]
+                    , value.split(",")[2]))
 
         else:
 
@@ -69,6 +72,7 @@ def create_song_list_each_name(names):
         users_history[name] = name_individual_songs
 
     return users_history
+
 
 def generate_leaderboard(leaderboard_length):
 
@@ -80,22 +84,25 @@ def generate_leaderboard(leaderboard_length):
     # create a list of each players songs in the same order as 'names_in_order'
     songs_scores_per_player_order = create_song_list_each_name(names_in_order)
 
-
     if ( len(list(all_players.values()) ) == 0 ):
-    # return placeholders if no players
-        return OrderedDict([('Log in to join the fun', ['No Completed Song Scores'])])
+        # return placeholders if no players
+        return OrderedDict([('Log in to join the fun'
+                        , ['No Completed Song Scores'])])
 
-    elif ( leaderboard_length == 0 or leaderboard_length >= len(names_in_order)):
+    elif ( leaderboard_length == 0
+            or leaderboard_length >= len(names_in_order)):
 
-        # Return all players and songs if called with 'leaderboard_length' of zero, if called
-        # with a 'leaderboard_length' higher than the number of names return all
+        # Return all players and songs if called with 'leaderboard_length'
+        # of zero, if called with a 'leaderboard_length' higher than the
+        # number of names return all
         return songs_scores_per_player_order
 
     else:
 
         # if the number of players is higher than the 'leaderboard_length'
         # return the amount requested
-        sliced = islice(songs_scores_per_player_order.items(), leaderboard_length)
+        sliced = islice(songs_scores_per_player_order.items()
+                        , leaderboard_length)
         sliced_o = OrderedDict(sliced)
 
         return sliced_o
